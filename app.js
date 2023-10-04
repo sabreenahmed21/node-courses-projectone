c0d8bb62ff7318284dca5afdc9bf79cfd4f07d20
sabreenahmed21 <sabreenahmed212@gmail.com>
2023-10-04 13:28:03 +0300

    second commit

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const httpStatusText = require("./utils/httpStatusText");
-const path = require('node:path');
+const path = require('path');
 
 app.use(cors());
 app.use(express.json());
